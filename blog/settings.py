eaeeeadcf4dcd539cc4ca4e8237b28c5845f6cea
Ubuntu <ubuntu@ip-172-31-30-27.eu-west-1.compute.internal>
2020-08-27 23:01:08 +0000

    settings middleware

--- blog/settings.py
+++ blog/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.midlleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
